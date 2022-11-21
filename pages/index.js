b865ead340d6317b175ec1d45b1ccdd9a0c568aa
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2022-11-21 20:46:30 +0100

    sass setup + nav

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Nav from '../components/Nav'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
+    <>
+    <Nav/>
     <div className={styles.container}>
       <Head>
         <title>HOP</title>
@@ -65,5 +68,6 @@ export default function Home() {
         </a>
       </footer>
     </div>
+    </>
   )
 }
