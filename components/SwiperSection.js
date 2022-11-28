04aa91b35829bae114d08cc202d52ffc17f18703
sofietrolle <112389843+sofietrolle@users.noreply.github.com>
2022-11-28 13:55:41 +0100

    --

--- components/SwiperSection.js
+++ components/SwiperSection.js
@@ -2,10 +2,12 @@
 /* eslint-disable react/display-name */
 import { useEffect, useState } from "react";
 import Article from "./Article";
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 import { Pagination, Navigation } from "swiper";
 import 'swiper/scss';
-import "swiper/scss/pagination"
+import "swiper/scss/pagination";
+// import "swiper/scss/navigation";
+
 
 export default () => {
     const [sectionData, setSectionData] = useState([]);
@@ -66,27 +68,36 @@ export default () => {
     //     }
     // });
 
+    // function SlideNextBtn() {
+    //     const useswiper = useSwiper();
+    //     return (
+    //         <button onClick={() => useswiper.slideNext()}>Next slide </button>
+    //     )
+    // }
+    
 
     return (
-        <Swiper
-        // spaceBetween={20}
-        slidesPerView={3}
-        // grabCursor={true}
-        // allowTouchMove={true}
-        // simulateTouch={true}
-        touchStartPreventDefault={false}
-        touchEventsTarget={".container"}
-        pagination={{ 
-            clickable: true,
-        }}
-        modules={[Pagination]}
+        <>
+        {/* <SlideNextBtn/> */}
+            <Swiper
+            // spaceBetween={20}
+            slidesPerView={2.75}
+            allowTouchMove={true}
+            touchStartPreventDefault={false}
+            touchEventsTarget={".container"}
+            pagination={{ 
+                clickable: true,
+            }}
+            navigation={true}
+            modules={[Pagination, Navigation]}
 
-        >
-            {sectionData.map((data) => (
-                <SwiperSlide key={data.id}>
-                    <Article data={data}/>
-                </SwiperSlide>
-            ))}
-        </Swiper>
+            >
+                {sectionData.map((data) => (
+                    <SwiperSlide key={data.id}>
+                        <Article data={data}/>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </>
     )
 } 
