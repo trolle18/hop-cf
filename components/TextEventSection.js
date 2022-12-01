9c903b3067adf20735e2ce24d80980c0d68ae40c
sofietrolle <112389843+sofietrolle@users.noreply.github.com>
2022-12-01 15:53:56 +0100

    --

--- components/TextEventSection.js
+++ components/TextEventSection.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import Image from "next/image";
-import CtaBtn from './CTAbtn';
 import CtaLink from './CtaLink';
+import MasonArticle from './MasonArticle';
 
 export default function TextEventSection() {
     const [sectionData, setSectionData] = useState([]);
@@ -35,39 +34,7 @@ export default function TextEventSection() {
 
                         <div className="mason-grid">
                             {section?.articles.map((article) => (
-
-                                <article key={article.id} className="mason-article">                                
-                                    <div className="mason-article__img-cntr">
-                                        {article?.img.map((img) => (
-                                            <Image key={img.id} src={img.src} alt={img.alt} width={100} height={100} />
-                                        ))}                              
-                                    </div>
-
-                                    <div className="mason-article__txt-cntr">
-                                        <div className='mason-article__txt-cntr__cont'>
-                                            <p className="mason-article__txt-cntr__cont__tag">{article.tag}</p>
-                                            <p className="mason-article__txt-cntr__cont__headline">{article.headline}</p>
-                                            {/* <div className='mason-article__txt-cntr__cont__link'> */}
-                                                {/* {article?.link.map((data) => (
-                                                    <a key={data.id} href={data.url}>{data.text}</a>
-                                                ))}  */}
-                                                {article?.link.map((data) => (
-                                                    <div key={data.id} className="arrow-right">
-                                                        <CtaLink key={data.id} data={data}/>
-                                                    </div>
-                                                ))} 
-                                            {/* </div>                                             */}
-                                        </div>    
-                                       
-
-                                        <div className="mason-article__txt-cntr__vertical-right">
-                                            <div className="mason-article__txt-cntr__vertical-right__inner-cntr">
-                                                <p>{article.dayMonth}</p><span></span><p>{article.year}</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </article> 
-
+                                <MasonArticle key={article.id} article={article} />
                             ))}   
                         </div>
 
